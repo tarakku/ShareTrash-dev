b4acf3623f48c19d601df22fc76573a955ad4d73
takumi <tarakku32b3@gmail.comm>
2025-06-27 14:17:07 +0900

    管理者ダッシュボード　お問い合わせ取得

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -7,11 +7,13 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Comment;
+use App\Models\Inquiry;
 
 class AdminDashboardController extends Controller
 {
     public function index()
     {
+        $inquiries = Inquiry::all();
         $posts = Post::all();
         $users = User::all();
         $comments = Comment::all();
@@ -20,6 +22,6 @@ class AdminDashboardController extends Controller
             'user_count' => $users->count(),
             'comment_count' => $comments->count(),
         ];
-        return view('admin.dashboard', compact('posts', 'users', 'stats'));
+        return view('admin.dashboard', compact('posts', 'users', 'stats','inquiries'));
     }
 }
