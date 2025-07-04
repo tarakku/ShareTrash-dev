0ac8bb01effebcce5016c4f6fdbccb2b54bf4520
takumi <tarakku32b3@gmail.comm>
2025-07-04 11:42:07 +0900

    register.cssのセレクタの範囲が広すぎてデザインが崩れる原因なので修正しました。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <title>@yield('title', 'ShareTrash')</title>
-    @vite(['resources/js/app.js',])
+    @vite(['resources/js/app.js'])
 </head>
 <body>
     <div class="page-wrapper" >
